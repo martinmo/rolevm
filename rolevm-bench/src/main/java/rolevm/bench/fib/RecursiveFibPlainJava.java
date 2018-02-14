e164980e16dbcdebc63d1f10009e3b4397251018
Martin Morgenstern <mail@mmorgenstern.net>
2018-02-14 09:06:42 +0100

    Benchmark RecursiveFibonacci with plain Java as well

---
+++ rolevm-bench/src/main/java/rolevm/bench/fib/RecursiveFibPlainJava.java
@@ -0,0 +1,47 @@
+package rolevm.bench.fib;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import rolevm.examples.fib.BenchmarkHelper;
+import rolevm.examples.fib.RecursiveFibonacci;
+
+/**
+ * Measures plain-Java Fibonacci performance, i.e., when the RoleVM agent wasn't
+ * loaded.
+ */
+@Fork(value = 1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class RecursiveFibPlainJava {
+    @State(Scope.Benchmark)
+    public static class Shared {
+        @Param({ "20" })
+        int n;
+    }
+
+    @State(Scope.Benchmark)
+    public static class WithoutCaching {
+        RecursiveFibonacci fib;
+
+        @Setup(Level.Iteration)
+        public void setupIteration() {
+            fib = new RecursiveFibonacci();
+        }
+    }
+
+    @Benchmark
+    public int without_caching(Shared shared, WithoutCaching state) {
+        return BenchmarkHelper.computeFib(state.fib, shared.n);
+    }
+}
--- rolevm-bench/src/main/java/rolevm/bench/fib/RecursiveFibWithAgent.java
+++ rolevm-bench/src/main/java/rolevm/bench/fib/RecursiveFibWithAgent.java
@@ -1,14 +1,8 @@
 package rolevm.bench.fib;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -19,26 +13,14 @@ import rolevm.examples.fib.FastFib;
 import rolevm.examples.fib.FastFib.CachedFibonacci;
 import rolevm.examples.fib.RecursiveFibonacci;
 
+/**
+ * Measures Fibonacci performance with a loaded RoleVM agent, with and without a
+ * bound {@link CachedFibonacci} role.
+ * 
+ * @author Martin Morgenstern
+ */
 @Fork(value = 1, jvmArgsAppend = { "@rolevm-bench/jvm.options" })
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
-public class RecursiveFibonacciBenchmark {
-    @State(Scope.Benchmark)
-    public static class Shared {
-        @Param({ "20" })
-        int n;
-    }
-
-    @State(Scope.Benchmark)
-    public static class WithoutCaching {
-        RecursiveFibonacci fib;
-
-        @Setup(Level.Iteration)
-        public void setupIteration() {
-            fib = new RecursiveFibonacci();
-        }
-    }
-
+public class RecursiveFibWithAgent extends RecursiveFibPlainJava {
     @State(Scope.Benchmark)
     public static class WithCaching {
         RecursiveFibonacci fib;
@@ -63,17 +45,7 @@ public class RecursiveFibonacciBenchmark {
     }
 
     @Benchmark
-    public void baseline() {
-        // empty
-    }
-
-    @Benchmark
     public int with_caching(Shared shared, WithCaching state) {
-        return BenchmarkHelper.computeFib(state.fib, shared.n);
-    }
-
-    @Benchmark
-    public int without_caching(Shared shared, WithoutCaching state) {
         return BenchmarkHelper.computeFib(state.fib, shared.n);
     }
 }
